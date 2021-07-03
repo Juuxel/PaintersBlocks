c1904003c80f7a4be0439e1b7b30bf64018872bf
Juuxel <6596629+Juuxel@users.noreply.github.com>
2021-07-03 18:38:08 +0300

    Use a cleverer inject to remove the need for sanity checks

--- src/main/java/juuxel/paintersblocks/mixin/ItemStackMixin.java
+++ src/main/java/juuxel/paintersblocks/mixin/ItemStackMixin.java
@@ -6,7 +6,6 @@
 
 package juuxel.paintersblocks.mixin;
 
-import juuxel.paintersblocks.PaintersBlocks;
 import juuxel.paintersblocks.item.PbItems;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -14,6 +13,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Slice;
 
 @Mixin(ItemStack.class)
 abstract class ItemStackMixin {
@@ -24,12 +24,12 @@ abstract class ItemStackMixin {
     private static native boolean isSectionVisible(int flags, ItemStack.TooltipSection tooltipSection);
 
     // would use an inject but isSectionVisible is static...
-    @Redirect(method = "getTooltip", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isSectionVisible(ILnet/minecraft/item/ItemStack$TooltipSection;)Z", ordinal = 2))
+    @Redirect(
+        method = "getTooltip",
+        at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isSectionVisible(ILnet/minecraft/item/ItemStack$TooltipSection;)Z", ordinal = 0),
+        slice = @Slice(from = @At(value = "FIELD", target = "Lnet/minecraft/item/ItemStack$TooltipSection;DYE:Lnet/minecraft/item/ItemStack$TooltipSection;"))
+    )
     private boolean hideDyeSectionForSwatches(int flags, ItemStack.TooltipSection section) {
-        if (section != ItemStack.TooltipSection.DYE) {
-            throw new AssertionError("[" + PaintersBlocks.ID + "] Injecting at an incorrect place!");
-        }
-
         if (getItem() == PbItems.SWATCH) {
             return false;
         }
