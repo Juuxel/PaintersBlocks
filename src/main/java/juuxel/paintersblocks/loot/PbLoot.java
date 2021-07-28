647641a2a63993298ca9e1bd0d3b1b2afb49e759
Juuz <6596629+Juuxel@users.noreply.github.com>
2021-07-29 02:54:48 +0300

    Fix #1

--- src/main/java/juuxel/paintersblocks/loot/PbLoot.java
+++ src/main/java/juuxel/paintersblocks/loot/PbLoot.java
@@ -7,14 +7,20 @@
 package juuxel.paintersblocks.loot;
 
 import juuxel.paintersblocks.PaintersBlocks;
+import net.minecraft.loot.function.LootFunctionType;
 import net.minecraft.loot.provider.nbt.LootNbtProviderType;
 import net.minecraft.util.registry.Registry;
 
 public final class PbLoot {
     public static final LootNbtProviderType PART = register("part", new LootNbtProviderType(new PartLootNbtProvider.Serializer()));
+    public static final LootFunctionType REMOVE_DEFAULT_COLOR = register("remove_default_color", new LootFunctionType(new RemoveDefaultColorLootFunction.Serializer()));
 
     private static LootNbtProviderType register(String id, LootNbtProviderType type) {
         return Registry.register(Registry.LOOT_NBT_PROVIDER_TYPE, PaintersBlocks.id(id), type);
+    }
+
+    private static LootFunctionType register(String id, LootFunctionType type) {
+        return Registry.register(Registry.LOOT_FUNCTION_TYPE, PaintersBlocks.id(id), type);
     }
 
     public static void init() {
